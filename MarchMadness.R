cb5adb7bcdddcb41b453770d4e3eae419543ebda
leandrag20 <leandragottschalk20@augustana.edu>
2023-03-14 20:28:42 -0500

    read the csv

--- MarchMadness.R
+++ MarchMadness.R
@@ -1,5 +1,10 @@
 library(readxl)
+library(dplyr)
 
 #Leandra's working directory
 setwd("~/git_data/marchMadness23")
 
+tournGameData <- read.csv('Tournament Game Data.csv')
+
+gonzagaData <- tournGameData %>%
+  dplyr::filter(TEAM == "Gonzaga")
