510a0445b1206ca79fb2f5e219d8117df3e2dfb0
leandrag20 <leandragottschalk20@augustana.edu>
2023-03-26 23:45:46 -0500

    adding comments and cleaning the r script

--- MarchMadness.R
+++ MarchMadness.R
@@ -18,18 +18,24 @@ rm(list=ls())
 
 #Mar 15
 
+#Reading the files into data frames
 tournGameData <- read.csv('data/Tournament Game Data.csv')
 tournGameDataClean <- tournGameData[!duplicated(tournGameData), ]
 tournData23 <- read.csv('data/2023 Tournament Data.csv')
 trendData <- read.csv('data/Tournament Trends.csv')
 saveRDS(trendData,'data/Tournament Game Data.rds')
-#clean data
+
+#clean data and putting it into tables
 table1 <- tournGameDataClean %>%
   select(YEAR,TEAM,SEED,TEAM.ROUND, FREE.THROW..)
 tournGameData23 <- table1%>%
   filter(YEAR == 2023)%>%
   select(TEAM,SEED)
+
+#removing duplicates from the table
 tournGameData23 <- tournGameData23[!duplicated(tournGameData23), ]
+
+#correcting the seeds for teams where it was wrong
 tournGameData23$SEED[tournGameData23$TEAM == "Maryland"] <- 8
 tournGameData23$SEED[tournGameData23$TEAM == "Furman"] <- 13
 tournGameData23$SEED[tournGameData23$TEAM == "Baylor"] <- 3
@@ -51,8 +57,12 @@ tournGameData23$SEED[tournGameData23$TEAM == "Illinois"] <- 9
 tournGameData23$SEED[tournGameData23$TEAM == "Iona"] <- 13
 tournGameData23$SEED[tournGameData23$TEAM == "Nevada"] <- 11
 tournGameData23$SEED[tournGameData23$TEAM == "NC-Asheville"] <- 15
+
+#filtering the seed and rounds
 table2 <- table1%>%
   filter(SEED<7, TEAM.ROUND>10)
+
+#creating more tables and removing non complete cases
 table3 <- trendData%>%
   select(TEAM,YEAR, NEUTRAL.WIN..)%>%
   group_by(YEAR)
@@ -60,16 +70,17 @@ table3<-table3[complete.cases(table3), ]
 table4 <- tournData23%>%
   select(TEAM, OFFENSIVE.REBOUND.., DEFENSIVE.REBOUND.., X2PT.., X3PT.., WIN.., FREE.THROW..)
 
+#merging the data into one table
 mergedData <- merge(tournGameData23, table4, by = "TEAM", all.x = TRUE)
 #Correcting the data to match the tournament accurately
 mergedData <- mergedData %>%
   add_row(TEAM = 'Princeton', OFFENSIVE.REBOUND.. = 28.7, DEFENSIVE.REBOUND.. = 77.3, SEED = 15, X2PT.. = 53.6, X3PT.. = 33.4, WIN.. = 72.4, FREE.THROW.. = 71.5)%>%
   add_row(TEAM = 'Penn State', OFFENSIVE.REBOUND.. = 19.2, DEFENSIVE.REBOUND.. = 74.4, SEED = 10, X2PT.. = 53.1, X3PT.. = 38.7, WIN.. = 62.2, FREE.THROW.. = 73.9)
 
+#creating a new column to predict based off of
+mergedData$Sum <- rowSums(mergedData[,3:8])
 
-#mergedData$`"AVERAGE"` <- rowMeans(mergedData$FREE.THROW.., mergedData$OFFENSIVE.REBOUND.., mergedData$DEFENSIVE.REBOUND.., mergedData$X2PT.., mergedData$X3PT..)
-mergedData$Average <- rowSums(mergedData[,3:8])
-#mergedData$Average <- mergedData$Average / 5
+#adding the regions to the table
 mergedData$REGION[mergedData$TEAM == "Alabama" | mergedData$TEAM == "Texas A&M-Corpus Christi" | mergedData$TEAM == "Maryland" | mergedData$TEAM == "West Virginia" |
                     mergedData$TEAM == "San Diego State" | mergedData$TEAM == "College of Charleston" | mergedData$TEAM == "Virginia" |
                     mergedData$TEAM == "Furman" | mergedData$TEAM == "Creighton" | mergedData$TEAM == "North Carolina State" | mergedData$TEAM == "Baylor" |
@@ -88,6 +99,7 @@ mergedData$REGION[mergedData$TEAM == "Kansas" | mergedData$TEAM == "Howard" | me
                     mergedData$TEAM == "Northwestern" | mergedData$TEAM == "Boise State" | mergedData$TEAM == "UCLA" | mergedData$TEAM == "NC-Asheville" | mergedData$TEAM == "Nevada"] <- "West"
 
 
+#creating a correlation model
 df_cortable<-mergedData%>%
   select(SEED, FREE.THROW.., OFFENSIVE.REBOUND.., DEFENSIVE.REBOUND.., X2PT.., X3PT.., WIN..)
 head(df_cortable)
@@ -95,6 +107,7 @@ B<-cor(df_cortable)
 head(round(B,2))
 corrplot(B, method="color")
 
+#testing out which variables give us the most ideal outcome
 table5 <- tournData23%>%
   select(TEAM, SEED, OFFENSIVE.REBOUND.., DEFENSIVE.REBOUND.., X3PT.., WIN..)
 table5 <- table5 %>%
@@ -119,7 +132,7 @@ table7 <- table7 %>%
 table7$Average <- rowSums(table7[,3:6])
 table7$Average <- table7$Average / 5
 
-
+#confirming our variables help predict upsets
 upsetReasoning1 <- mergedData%>%
   dplyr::filter(TEAM == "Furman" | TEAM == "Virginia")
 
